fdfaf866742ac0b48f9ed7e49b68be064029878f
Rodrigo Ochoa <rodrigrovelasquez@gmail.com>
2026-03-19 08:54:39 +0100

    ACTUALIZADO

--- src/section/Qsomos.tsx
+++ src/section/Qsomos.tsx
@@ -10,7 +10,7 @@ const QSomos = () => {
   //bloque de la funcion
   return (
     <section id="qsomos" className="min-h-screen flex items-center justify-center scroll-mt-16">
-        <h1>CANAVIS ES VIDA </h1>
+        <h1>Cannavida </h1>
     </section>
   )
 }
